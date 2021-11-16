2d8e639d3765093975d8b59e7c5ade88dd90bafc
sergiosaad <saad@saad.inf.br>
2021-11-16 10:30:17 +0100

    improving example connection

--- example/db_config.php
+++ example/db_config.php
@@ -4,9 +4,9 @@ define("DATA_LAYER_CONFIG", [
     "driver" => "mysql",
     "host" => "localhost",
     "port" => "3306",
-    "dbname" => "daviw",
-    "username" => "root",
-    "passwd" => "",
+    "dbname" => "dwdatalayer",
+    "username" => "usrdwdtlayer",
+    "passwd" => "passdwdtlaye",
     "options" => [
         PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
