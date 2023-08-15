98879c17581876d8a2eec8101f537359fa1970ad
sergiosaad <saad@arqmedes.com>
2023-08-15 21:17:41 +0200

    enterprise example refined

--- example/Models/Enterprise.php
+++ example/Models/Enterprise.php
@@ -27,18 +27,24 @@ class Enterprise extends DataLayer
 
 //Enterprise table example
   
-//   CREATE TABLE IF NOT EXISTS `tb_enterprise` (
-//   `co_seq_enterprise` INT NOT NULL AUTO_INCREMENT,
-//   `nm_enterprise` VARCHAR(80) NOT NULL,
-//   `ds_enterprise` VARCHAR(400) NULL,
-//   `lk_enterprise_logo` VARCHAR(400) NULL,
-//   `lk_enterprise_thumb` VARCHAR(400) NULL,
-//   `lk_enterprise_skin` VARCHAR(400) NULL,
-//   `dt_created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-//   `nm_created` VARCHAR(80) NOT NULL DEFAULT 'automatic',
-//   `dt_edited` DATETIME NULL DEFAULT NULL,
-//   `nm_edited` VARCHAR(80) NULL DEFAULT NULL,
-//   `st_record` INT(11) NOT NULL DEFAULT 1,
-//    PRIMARY KEY (`co_seq_enterprise`))
-//    ENGINE = InnoDB
-  
+// CREATE TABLE IF NOT EXISTS `tb_enterprise` (
+//     `co_seq_enterprise` INT NOT NULL AUTO_INCREMENT,
+//     `co_enterprise` INT NULL,
+//     `nm_enterprise` VARCHAR(80) NOT NULL,
+//     `ds_enterprise` VARCHAR(400) NULL,
+//     `lk_enterprise_logo` VARCHAR(400) NULL,
+//     `lk_enterprise_thumb` VARCHAR(400) NULL,
+//     `lk_enterprise_skin` VARCHAR(400) NULL,
+//     `dt_created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+//     `nm_created` VARCHAR(80) NOT NULL DEFAULT 'automatic',
+//     `dt_edited` DATETIME NULL DEFAULT NULL,
+//     `nm_edited` VARCHAR(80) NULL DEFAULT NULL,
+//     `st_record` INT(11) NOT NULL DEFAULT 1,
+//     PRIMARY KEY (`co_seq_enterprise`),
+//     INDEX `fk_rsk_tb_enterprise_rsk_tb_enterprise1_idx` (`co_enterprise` ASC),
+//     CONSTRAINT `fk_rsk_tb_enterprise_rsk_tb_enterprise1`
+//       FOREIGN KEY (`co_enterprise`)
+//       REFERENCES `tb_enterprise` (`co_seq_enterprise`)
+//       ON DELETE NO ACTION
+//       ON UPDATE NO ACTION)
+//   ENGINE = InnoDB
--- example/enterprise_example.php
+++ example/enterprise_example.php
@@ -13,20 +13,28 @@ $now = date_format((new DateTimeImmutable()), 'Y-m-d H:i:s');
  * MODEL
  */
 print "\n-- model ----------------------------\n\n";
-$model = new Enterprise();
+$enterprise1 = new Enterprise();
+$enterprise2 = new Enterprise();
 
 /*
  * CREATE AND EDIT
  */
 
 print "\n-- save ----------------------------\n\n";
-$model->nm_enterprise = "New Enterprise - $now";
-$model->ds_enterprise = "New Enterprise - $now";
-$model->lk_enterprise_skin = "http://www.arqmedes.com";
-$model->save();
-$model->nm_edited = "saad@arqmedes.com";
-$model->save();
-print_r($model);
+$enterprise1->nm_enterprise = "New Enterprise - $now";
+$enterprise1->ds_enterprise = "New Enterprise - $now";
+$enterprise1->lk_enterprise_skin = "http://www.arqmedes.com";
+$enterprise1->save();
+$enterprise1->nm_edited = "saad@arqmedes.com";
+$enterprise1->save();
+print_r($enterprise1->data());
+
+$enterprise2->nm_enterprise = "Enterprise child - $now";
+$enterprise2->co_enterprise = $enterprise1->co_seq_enterprise;
+$enterprise2->ds_enterprise = "Enterprise child description - $now";
+$enterprise2->lk_enterprise_skin = "http://www.arqmedesconsultoria.com.br";
+$enterprise2->save();
+print_r($enterprise2->data());
 
 print "\n-- findById ----------------------------\n\n";
 
